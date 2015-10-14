6dd37b0a0111df7fa2ef45bce0d020ed45fe0fa4
MichaelPeterMacahilig11035060 <michael.p.macahilig@student.uts.edu.au>
2015-10-14 16:47:57 +1100

    added mongolab uri

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 var config = {};
 
-config.mongoUri = 'mongodb://localhost:27017/admin-communitycloud';
+config.mongoUri = 'mongodb://heroku_84pm1q8m:m9cucg2ahm29tb8oc0faanofau@ds053818.mongolab.com:53818/heroku_84pm1q8m/admin-communitycloud';
 
 config.cookieMaxAge = 90 * 24 * 3600 * 1000;
 
