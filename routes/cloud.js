a18c6d760b76eeb322f00f4df1204b5ae673fbb4
MichaelPeterMacahilig11035060 <michael.p.macahilig@student.uts.edu.au>
2015-10-27 16:11:19 +1100

    changed array to json

--- routes/cloud.js
+++ routes/cloud.js
@@ -19,9 +19,11 @@ router.get('/', restrict, function(req, res, next) {
 
     AlertNotification.find({UserId: req.user._id},function(err,docs){
         var alertArray = [];
+        var i = 0;
         docs.forEach(function(err,alerts){
             Alert.find({_id:alerts.alertId},function(err,alert){
-                alertArray.push(alert);
+                alertArray[i] = alert;
+                i++;
             });
         });
         alertArray = JSON.stringify(alertArray);
